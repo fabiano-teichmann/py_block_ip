4e8e47efdf4d46e82a2c34a193082e12d0a3c33a
Fabiano Teichmann <fabiano.geek@gmail.com>
2019-05-11 18:39:39 -0300

    Create class control iptables

--- block_access.py
+++ block_access.py
@@ -1,25 +1,20 @@
 import iptc
 
 
-def block(ip):
-    rule = iptc.Rule()
-    rule.in_interface = "eth0"
-    rule.src = "192.168.1.0/255.255.255.0"
+class ControlIptables(object):
+    def __init__(self):
+        self.rule = iptc.Rule()
+        self.rule.in_interface = "eth0"
+        self.chain = iptc.Chain(iptc.Table(iptc.Table.FILTER), "INPUT")
 
+    def lock_ip(self, ip):
+        self.rule.src = ip
+        target = self.rule.create_target("DROP")
+        self.rule.target = target
+        self.chain.insert_rule(self.rule)
 
-# chain = iptc.Chain(iptc.Table(iptc.Table.FILTER), "INPUT")
-# >>> chain.insert_rule(rule)
-# >>> arget = rule.create_target("DROP")
-# >>> rule.src = "192.168.25.235"
-# >>> target = iptc.Target(rule, "DROP")
-# >>> rule.target = target
-# >>> chain.insert_rule(rule)
-# >>> target = rule.create_target("DROP")
-# >>> target
-# <iptc.ip4tc.
-
-"""
-check path = lambda x, y: True if x in y else False
-list(map(check path , paths, path))
-
-"""
+    def unlock_ip(self, ip):
+        self.rule.src = ip
+        target = self.rule.create_target("ACCEPT")
+        self.rule.target = target
+        self.chain.insert_rule(self.rule)
