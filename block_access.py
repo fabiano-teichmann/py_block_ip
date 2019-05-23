7a3f52d8b70a39ca5315ae1872c3122d304dbe27
Fabiano Teichmann <fabiano.geek@gmail.com>
2019-05-22 22:57:35 -0300

    Create rules for block subnet

--- block_access.py
+++ block_access.py
@@ -2,12 +2,15 @@ import iptc
 
 
 class ControlIptables(object):
-    def __init__(self):
+    def __init__(self, interface=False):
         self.rule = iptc.Rule()
-        self.rule.in_interface = "eth0"
+        if interface:
+            self.rule.in_interface = interface
         self.chain = iptc.Chain(iptc.Table(iptc.Table.FILTER), "INPUT")
 
-    def lock_ip(self, ip):
+    def lock_ip(self, ip, subnet=False):
+        if subnet:
+            ip = subnet
         self.rule.src = ip
         target = self.rule.create_target("DROP")
         self.rule.target = target
@@ -18,3 +21,10 @@ class ControlIptables(object):
         target = self.rule.create_target("ACCEPT")
         self.rule.target = target
         self.chain.insert_rule(self.rule)
+
+    def ip_and_subnet(self, ip, subnet):
+        subnet_split = subnet.split('/')
+        ip_split = ip.split('.')
+        ip = ip_split[:-len(subnet_split[0])]
+        ip = ".".join(ip)
+        return f"{ip}.{subnet}"
